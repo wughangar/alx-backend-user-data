5fdb496e8be34dd6e4a0bee37ac0b8255b0a98d1
wughangar <wughangar.rose@gmail.com>
2024-01-16 07:35:40 +0000

    error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized() -> str:
+    """ raise 401 unauthorized error"""
+    abort(401)
