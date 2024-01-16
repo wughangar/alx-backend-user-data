710d0699d66860c6db6fbc1facec484810161f30
wughangar <wughangar.rose@gmail.com>
2024-01-16 14:03:22 +0000

    authorization

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,7 @@ class Auth:
         if request is None or 'Authorization' not in request.headers:
             return None
         else:
-            return request.headers['Authorization']
+            return request.headers.get['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
