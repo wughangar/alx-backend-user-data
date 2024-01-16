ae5ca2d22ce208e29e438e9d94fe837fd19d914e
wughangar <wughangar.rose@gmail.com>
2024-01-16 09:41:05 +0000

    auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 """
 
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
@@ -11,4 +12,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ 
         public method
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        public method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        public method
+        """
+        return None
+
+
 
