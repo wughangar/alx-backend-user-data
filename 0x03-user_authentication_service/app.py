f89880972f58217deee7a9e0e7ebd1d092f50b6a
wughangar <wughangar.rose@gmail.com>
2024-01-24 05:37:12 +0000

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,10 @@
 """
 basic flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -11,6 +14,20 @@ app = Flask(__name__)
 def welcome():
     """ function returns jsonified string"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def register_user():
+    """ function that registers the user"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        new_user = AUTH.register_user(email, password)
+        return jsonify(
+                {"email": new_user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
