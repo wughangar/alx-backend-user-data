98078d9725e22b9d5b70c4a388a55bfd7201f5a4
wughangar <wughangar.rose@gmail.com>
2024-01-24 09:58:53 +0000

    existing_user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,15 +54,13 @@ def login() -> str:
 @app.route('/sessions', methods=['DELETE'])
 def logout() -> str:
     session_id = request.cookies.get('session_id')
-    if session_id is None:
-        abort(403)
-    user = AUTH.get_user_from_session_id(session_id)
+    existing_user = AUTH.get_user_from_session_id(session_id)
 
-    if user is not None:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
+    if existing_user is None:
         abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == "__main__":
