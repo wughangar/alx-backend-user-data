611178e300f65acdb44b117eac975fa0910fd469
wughangar <wughangar.rose@gmail.com>
2024-01-25 07:04:55 +0000

    update password function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,5 +84,21 @@ def reset_password() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """
+    function to update user's password using a reset token
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError as e:
+        abort(403, str(e))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
