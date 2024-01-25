7ac30fc4fb23fd48845085d0b9e533075223e172
wughangar <wughangar.rose@gmail.com>
2024-01-25 06:48:51 +0000

    chNGED LOGIC

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -119,8 +119,11 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            if user:
+                reset_token = str(uuid.uuid4())
+                self._db.update_user(user.id, reset_token=reset_token)
+                return reset_token
+            else:
+                raise ValueError(f"User with email {email} not found.")
         except NoResultFound:
             raise ValueError(f"User with email {email} not found.")
-        reset_token = str(uuid.uuid4())
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
